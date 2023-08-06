c643414aefc6996c174609ff72099d99e6bd413b
Walany Costa <walanybnegro@gmail.com>
2023-08-07 00:05:42 +0100

    feat: create ZodValidator to validate fields

--- src/main/factory/login-factory/login-factory.tsx
+++ src/main/factory/login-factory/login-factory.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { Login } from '../../../presentation/screen/login'
 import { FirebaseAuthentication } from '../../../domain/services/firebase-authentication'
 import { SignInWithPassword } from '../../../infra/firebase/authentication/signin-with-password'
+import { ZodValidatior } from '../../../validation/zod-validator'
 
 const makeSignInPassword = new SignInWithPassword()
 const makeFirebaseAuthentication = new FirebaseAuthentication(makeSignInPassword)
+const makeZodValidation = new ZodValidatior()
 
 export function LoginFactory () {
   return (
-    <Login authentication={makeFirebaseAuthentication}/>
+    <Login
+      validator={makeZodValidation}
+      authentication={makeFirebaseAuthentication}
+    />
   )
 }
---
+++ src/validation/zod-validator.ts
@@ -0,0 +1,26 @@
+import * as z from 'zod'
+import { Validator } from '../presentation/protocols/validator'
+
+export class ZodValidatior implements Validator {
+  private readonly validateSchema: any
+
+  constructor () {
+    this.validateSchema = z.object({
+      email: z.string({ required_error: 'Digite a inteligencia' })
+        .email({ message: 'Email inválido. Por favor passe email correcto' })
+        .optional(),
+      password: z.string()
+        .min(6, { message: 'A senha deve ter conter pelomenos 6 caracteres' })
+        .optional()
+    })
+  }
+
+  validate (fieldName: string, value: string): string {
+    try {
+      this.validateSchema.parse({ [fieldName]: value })
+      return ''
+    } catch (error) {
+      return JSON.parse(error)[0].message
+    }
+  }
+}
