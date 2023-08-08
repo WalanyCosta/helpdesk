386b758d2a1226a70f2e5f26bfcb58ccc07ccad9
Walany Costa <walanybnegro@gmail.com>
2023-08-08 16:26:08 +0100

    chore: add react-navigation of the type stack

--- src/main/routes/route.tsx
+++ src/main/routes/route.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import { LoginFactory } from '../factory/login-factory/login-factory'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
+import { View, Text, Button } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export function Routes () {
   return (
-    <LoginFactory />
+       <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="register" component={Register} />
+        <Stack.Screen name="login" component={LoginFactory} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  )
+}
+
+function Register () {
+  const navigation = useNavigation()
+  return (
+    <View>
+      <Text>Screen Register</Text>
+      <Button title='ir ao login' onPress={() => { navigation.navigate('login') }}/>
+    </View>
   )
 }
