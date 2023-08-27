bb037f27fbe9578d6fc9a64f6e844af40f86721f
Walany Costa <walanybnegro@gmail.com>
2023-08-27 05:43:09 +0100

    feat: add dependecy RemoteLoadCalls in the HomeFactory

--- src/main/factory/home-factory/home-factory.tsx
+++ src/main/factory/home-factory/home-factory.tsx
@@ -3,16 +3,19 @@ import { ZodValidation } from '../../../validation/zod-validator'
 import { Home } from '../../../presentation/screen/home'
 import { CloudSaveCall } from '../../../domain/services/cloud-save-call'
 import { CloudFirebaseStore } from '../../../infra/firebase/firestore/cloud-firestore'
+import { RemoteLoadCalls } from '../../../domain/services/remote-load-calls'
 
 const makeZodValidation = new ZodValidation()
 const makeFirebaseStore = new CloudFirebaseStore()
 const makeCloudSaveStore = new CloudSaveCall(makeFirebaseStore)
+const makeRemoteLoadCalls = new RemoteLoadCalls(makeFirebaseStore)
 
 export function HomeFactory () {
   return (
     <Home
       validation={makeZodValidation}
       saveCall={makeCloudSaveStore}
+      loadCalls={makeRemoteLoadCalls}
     />
   )
 }
