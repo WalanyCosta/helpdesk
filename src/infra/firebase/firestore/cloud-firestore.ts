443e8dd72a21a68415096fcafb49fe662efd00b5
Walany Costa <walanybnegro@gmail.com>
2023-08-27 05:33:27 +0100

    feat: add method  getCalls in CloudFirestore to find all data in enttity call

--- src/infra/firebase/firestore/cloud-firestore.ts
+++ src/infra/firebase/firestore/cloud-firestore.ts
@@ -1,9 +1,11 @@
 import { CloudSaveCallParam, CloudSaveStore } from '../../../domain/protocols/cloud-save-store'
-import { getFirestore, addDoc, collection } from 'firebase/firestore'
+import { getFirestore, addDoc, collection, getDocs } from 'firebase/firestore'
 import { app } from '../config/app'
+import { FindAllCall } from '../../../domain/protocols/find-all-call'
 
-export class CloudFirebaseStore implements CloudSaveStore {
+export class CloudFirebaseStore implements CloudSaveStore, FindAllCall {
   private readonly connectionWithFirestore = getFirestore(app)
+
   async save (param: CloudSaveCallParam): Promise<any> {
     try {
       await addDoc(collection(this.connectionWithFirestore, 'call'), {
@@ -12,5 +14,26 @@ export class CloudFirebaseStore implements CloudSaveStore {
     } catch (error) {
 
     }
+  }
+
+  async getAll (): Promise<any> {
+    const refCollection = collection(this.connectionWithFirestore, 'call')
+    try {
+      const response = await getDocs(refCollection)
+      return this.mapDatas(response)
+    } catch (error) {
+      return error.code
+    }
+  }
+
+  private mapDatas (response): any {
+    const calls = []
+    response.forEach((doc) => {
+      calls.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
+    return calls
   }
 }
