3a2212949b7e2ec9435783e481e2027096925b2e
Walany Costa <walanybnegro@gmail.com>
2023-08-28 03:25:21 +0100

    feat: add loading to indicate calls they's load

--- src/presentation/screen/home/component/calls/index.tsx
+++ src/presentation/screen/home/component/calls/index.tsx
@@ -8,6 +8,7 @@ import { Call } from '../call'
 import { LoadCalls } from '../../../../../domain/protocols/load-calls'
 import { CallModel } from '../../../../../domain/model/calls'
 import { FlatList } from 'react-native'
+import { Loading } from '../../../../components/Animations/loading'
 
 type Props = {
   loadCalls: LoadCalls
@@ -15,25 +16,34 @@ type Props = {
 }
 
 export function Calls ({ handleSnapPress, loadCalls }: Props) {
-  const [call, setCalls] = useState([] as CallModel[])
+  const [loading, isLoading] = useState(false)
+  const [call, setCalls] = useState<CallModel[]>([])
 
   useEffect(() => {
+    isLoading(true)
+
     loadCalls.load()
       .then((data) => { setCalls(data) })
       .catch((error) => { console.log(error.message) })
+      .finally(() => { isLoading(false) })
   }, [])
 
   return (
     <Container>
         <SubTitle>Meus Chamados</SubTitle>
-        <FlatList
-          keyExtractor={(item) => item.id}
-          data={call}
-          renderItem={({ item }) => <Call data={item} key={item.id} onPress={handleSnapPress}/>}
-          contentContainerStyle={{ paddingBottom: 100 }}
-          showsVerticalScrollIndicator={false}
-          style={{ flex: 1 }}
-        />
+        {
+          loading
+            ? <Loading />
+            : <FlatList
+              keyExtractor={(item) => item.id}
+              data={call}
+              renderItem={({ item }) => <Call data={item} key={item.id} onPress={handleSnapPress}/>}
+              contentContainerStyle={{ paddingBottom: 100 }}
+              showsVerticalScrollIndicator={false}
+              style={{ flex: 1 }}
+            />
+
+        }
     </Container>
   )
 }
